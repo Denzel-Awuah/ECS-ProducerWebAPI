d78612c5605bcd02b7063ca474d8db1ebc3c10f2
Denzel Awuah <denzelbawuah@gmail.com>
2025-02-17 12:18:57 -0500

    upated deployment

--- obj/Debug/net8.0/ECS-ProducerWebAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ECS-ProducerWebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECS-ProducerWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d9d8aaa2f8acd2a9c702e8b29f134d306efe4f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8552e0d27897251a7713ad579e53c7cd08e96adf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECS-ProducerWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECS-ProducerWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
