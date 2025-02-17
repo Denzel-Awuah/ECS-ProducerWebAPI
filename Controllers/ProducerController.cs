16902133521f4e85fe0c57e3b20c090b357ba256
Denzel Awuah <denzelbawuah@gmail.com>
2025-02-17 09:58:48 -0500

    first commit

---
+++ Controllers/ProducerController.cs
@@ -0,0 +1,29 @@
+﻿using Amazon.SQS;
+using ECS_ProducerWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECS_ProducerWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProducerController : ControllerBase
+    {
+
+        private IAmazonSQS sqsClient;
+
+        public ProducerController(IAmazonSQS sqsClient)
+        {
+            this.sqsClient = sqsClient;
+        }
+
+        [HttpPost]
+        public Task<IActionResult> SendMessage([FromBody] Message message)
+        {
+            //Send the message to Queue 
+
+            return Ok("Message Sent");
+        }
+
+    }
+}
